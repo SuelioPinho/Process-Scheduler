a4cdbfeef5aec9ba09371d523dd2b3a9c7abcd6f
sueliopss <sueliopss@172.18.9.199>
2016-03-16 17:33:47 -0300

    Horizontal Scroll GridView

--- app/src/main/java/com/example/sueliopss/escalonador/MainActivity.java
+++ app/src/main/java/com/example/sueliopss/escalonador/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.sueliopss.escalonador;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.GridView;
-import android.widget.ListView;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import org.androidannotations.annotations.AfterViews;
@@ -31,12 +37,15 @@ public class MainActivity extends AppCompatActivity {
     @Bean
     ProcessadorAdapter processadorAdapter;
 
+    @ViewById
+    HorizontalScrollView horizontalScrollView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
+        //createDialog(savedInstanceState).show();
 
 
     }
@@ -47,6 +56,8 @@ public class MainActivity extends AppCompatActivity {
 
         List<Integer> images = new ArrayList<>();
 
+
+
         images.add(R.mipmap.ic_insert_chart_black_24dp);
         images.add(R.mipmap.ic_insert_chart_black_24dp);
         images.add(R.mipmap.ic_insert_chart_black_24dp);
@@ -69,12 +80,14 @@ public class MainActivity extends AppCompatActivity {
         images.add(R.mipmap.ic_insert_chart_black_24dp);
         images.add(R.mipmap.ic_insert_chart_black_24dp);
 
-
-
+        gridProcessadores.setNumColumns(images.size());
+        gridViewSetting(gridProcessadores, images.size());
 
 
         processadorAdapter.setImageList(images);
         gridProcessadores.setAdapter(processadorAdapter);
+
+
     }
 
     @Override
@@ -107,6 +120,40 @@ public class MainActivity extends AppCompatActivity {
     @ItemClick(R.id.gridProcessadores)
     public void alert(int position){
         Toast.makeText(this,(position+1)+"",Toast.LENGTH_SHORT).show();
+    }
+
+    public Dialog createDialog(Bundle savedInstanceState){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        LayoutInflater layoutInflater = this.getLayoutInflater();
+
+        builder.setView(layoutInflater.inflate(R.layout.dialog_main_escalonador, null));
+
+        //builder.setCancelable(false);
+
+        return builder.create();
+    }
+
+    private void gridViewSetting(GridView gridview, int size) {
+
+        // Calculated single Item Layout Width for each grid element ....
+        int width = 50 ;
+
+        DisplayMetrics dm = new DisplayMetrics();
+        this.getWindowManager().getDefaultDisplay().getMetrics(dm);
+        float density = dm.density;
+
+        int totalWidth = (int) (width * size * density);
+        int singleItemWidth = (int) (width * density);
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                totalWidth, LinearLayout.LayoutParams.MATCH_PARENT);
+
+        gridview.setLayoutParams(params);
+        gridview.setColumnWidth(singleItemWidth);
+        gridview.setHorizontalSpacing(2);
+        gridview.setStretchMode(GridView.STRETCH_SPACING);
+        gridview.setNumColumns(size);
     }
 
 }
